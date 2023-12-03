d31d653d725ccc408cab91e0c21867abc77b32be
Ben Wainwright <bwainwright28@gmail.com>
2023-12-03 18:08:51 +0000

    fix weird scroll thing

--- src/components/containers/tab-box/tab-box.css.ts
+++ src/components/containers/tab-box/tab-box.css.ts
@@ -1,13 +1,12 @@
-import { style } from '@vanilla-extract/css';
+import { style } from "@vanilla-extract/css";
 
 export const buttonRow = style({
-  overflow: 'scroll',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  width: '100%',
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  width: "100%",
 });
 
 export const tabContainer = style({
-  width: '100%',
+  width: "100%",
 });
