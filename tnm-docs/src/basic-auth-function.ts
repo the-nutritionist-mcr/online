c5959bdddc19b68079958706c95d11fd237f4157
Ben Wainwright <bwainwright28@gmail.com>
2023-12-16 11:02:39 +0000

    add basic auth to docs

---
+++ tnm-docs/src/basic-auth-function.ts
@@ -0,0 +1,31 @@
+export const makeBasicAuthFunction = (
+  user: string | undefined,
+  password: string | undefined
+) => `
+function handler(event) {
+  var request = event.request;
+  var headers = request.headers;
+
+  const authUser = "${user}";
+  const authPass = "${password}";
+  var authString = "Basic " + (authUser + ":" + authPass).toString("base64");
+
+  if (
+    typeof headers.authorization == "undefined" ||
+    headers.authorization.value != authString
+  ) {
+    var response = {
+      statusCode: 401,
+      statusDescription: "Unauthorized",
+      headers: {
+        "www-authenticate": {
+          value: 'Basic realm="Enter credentials."',
+        },
+      },
+    };
+    return response;
+  }
+
+  return request;
+}
+`;
--- tnm-docs/src/infrastructure.ts
+++ tnm-docs/src/infrastructure.ts
@@ -1,6 +1,11 @@
 import { App, Stack } from "aws-cdk-lib";
 import { DnsValidatedCertificate } from "aws-cdk-lib/aws-certificatemanager";
-import { Distribution } from "aws-cdk-lib/aws-cloudfront";
+import {
+  Distribution,
+  Function,
+  FunctionCode,
+  FunctionEventType,
+} from "aws-cdk-lib/aws-cloudfront";
 import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 import {
   ARecord,
@@ -12,6 +17,7 @@ import { Bucket, BucketAccessControl } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
 import path from "path";
+import { makeBasicAuthFunction } from "./basic-auth-function";
 
 export class DocsStack extends Stack {
   constructor(scope: Construct, id: string) {
@@ -50,9 +56,24 @@ export class DocsStack extends Stack {
       region: "us-east-1",
     });
 
+    const authFunction = new Function(this, "auth-function", {
+      code: FunctionCode.fromInline(
+        makeBasicAuthFunction(
+          process.env["BASIC_AUTH_USERNAME"],
+          process.env["BASIC_AUTH_PASSWORD"]
+        )
+      ),
+    });
+
     const distribution = new Distribution(this, "tnm-web-distribution", {
       defaultBehavior: {
         origin: new S3Origin(assetsBucket),
+        functionAssociations: [
+          {
+            function: authFunction,
+            eventType: FunctionEventType.VIEWER_REQUEST,
+          }
+        ]
       },
       certificate,
       domainNames: [domainName],
