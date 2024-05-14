e917199fce97fa8be23049c2f2e326d42f11150b
Adam Palmer <ad@palmerama.tv>
2024-05-14 16:34:49 +0100

    code cleanup

--- src/backend/lambdas/handlers/chargebee-get-customer.ts
+++ src/backend/lambdas/handlers/chargebee-get-customer.ts
@@ -11,8 +11,6 @@ import {
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   try {
     await protectRoute(event, ["admin"]);
-
-    // const id = '77ECsTyblGXbD2P'; 
     const payload = JSON.parse(event.body ?? '');
   
     const result = await new Promise<typeof chargebee.customer>(
@@ -42,58 +40,3 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     return returnErrorResponse(error)
   }
 }
-
-
-
-// chargebee customer: {
-//   "id": "77ECsTyblGXbD2P",
-//   "first_name": "Addy",
-//   "last_name": "Palmer",
-//   "email": "adam@thenutritionistmcr.com",
-//   "auto_collection": "on",
-//   "net_term_days": 0,
-//   "allow_direct_debit": false,
-//   "created_at": 1702732791,
-//   "created_from_ip": "77.97.132.147",
-//   "taxability": "taxable",
-//   "updated_at": 1705673191,
-//   "locale": "en",
-//   "pii_cleared": "active",
-//   "channel": "web",
-//   "resource_version": 1705673191326,
-//   "deleted": false,
-//   "object": "customer",
-//   "billing_address": {
-//     "first_name": "Adamus",
-//     "last_name": "Palmer",
-//     "email": "adam@thenutritionistmcr.com",
-//     "company": "The Nutritionist",
-//     "phone": "+44075153566235",
-//     "line1": "64 Strawberry Fields",
-//     "city": "Liverpool",
-//     "country": "GB",
-//     "zip": "L26 7FJ",
-//     "validation_status": "not_validated",
-//     "object": "billing_address"
-//   },
-//   "card_status": "valid",
-//   "promotional_credits": 0,
-//   "refundable_credits": 0,
-//   "excess_payments": 0,
-//   "unbilled_charges": 0,
-//   "preferred_currency_code": "GBP",
-//   "mrr": 0,
-//   "primary_payment_source_id": "pm_BTLzEhTyby5JOEX7",
-//   "payment_method": {
-//     "object": "payment_method",
-//     "type": "card",
-//     "reference_id": "tok_BTLzEhTyby5JCEX6",
-//     "gateway": "chargebee",
-//     "gateway_account_id": "gw_199LVfSrH8SXXo",
-//     "status": "valid"
-//   },
-//   "tax_providers_fields": [],
-//   "cf_cook_1_delivery_day": "Monday",
-//   "cf_cook_2_delivery_day": "Friday",
-//   "cf_number_of_bags": 4
-// }
