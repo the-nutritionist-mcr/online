b88f5f36911180c06eb33ea94c4b7fc3eba61454
Ben Wainwright <bwainwright28@gmail.com>
2026-04-01 13:30:01 +0100

    remove try/finally block

--- src/backend/event-handlers/updated-handled-subscription-resumed.ts
+++ src/backend/event-handlers/updated-handled-subscription-resumed.ts
@@ -15,35 +15,32 @@ export const updatedHandledSubscriptionResumed = async (
 ) => {
   const subscriptionId = event.content.subscription.id;
   const subscriptionMrr = event.content.subscription.mrr;
-  try {
-    const pauseStart = getPauseDate(event);
-    const resumeDate = DateTime.fromSeconds(event.occurred_at);
+  const pauseStart = getPauseDate(event);
+  const resumeDate = DateTime.fromSeconds(event.occurred_at);
 
-    if (!subscriptionMrr) {
-      return;
-    }
-
-    const invoice = await getInvoiceContainingDate(
-      client,
-      subscriptionId,
-      pauseStart
-    );
-
-    const pauseCredit = calculatePauseCredit({
-      pauseStart,
-      resumeDate,
-      subscriptionMrr,
-    });
-
-    await applyCreditNoteToInvoice(client, {
-      credit: pauseCredit,
-      invoice,
-      reason: SUBSCRIPTION_PAUSED_REASON_CODE,
-      notes: `Pause credit for ${pauseCredit.creditDays} day${
-        pauseCredit.creditDays === 1 ? "" : "s"
-      }.`,
-    });
-  } finally {
-    await setPauseDate(client, subscriptionId, null);
+  if (!subscriptionMrr) {
+    return;
   }
+
+  const invoice = await getInvoiceContainingDate(
+    client,
+    subscriptionId,
+    pauseStart
+  );
+
+  const pauseCredit = calculatePauseCredit({
+    pauseStart,
+    resumeDate,
+    subscriptionMrr,
+  });
+
+  await applyCreditNoteToInvoice(client, {
+    credit: pauseCredit,
+    invoice,
+    reason: SUBSCRIPTION_PAUSED_REASON_CODE,
+    notes: `Pause credit for ${pauseCredit.creditDays} day${
+      pauseCredit.creditDays === 1 ? "" : "s"
+    }.`,
+  });
+  await setPauseDate(client, subscriptionId, null);
 };
