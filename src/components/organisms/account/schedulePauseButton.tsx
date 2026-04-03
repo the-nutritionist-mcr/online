86e1a7f3980b6e88e091cf18e1e00902a5824699
Ben Wainwright <bwainwright28@gmail.com>
2026-04-03 12:22:02 +0100

    correct loading mistake

--- src/components/organisms/account/schedulePauseButton.tsx
+++ src/components/organisms/account/schedulePauseButton.tsx
@@ -1,10 +1,10 @@
-import { FC, useEffect, useState } from 'react';
-import { apiRequest } from '../../../core/api-request';
-import { BackendCustomer } from '@tnmo/types';
-import MainButton from '@/components/ui/main-button';
-import { DateTime } from 'luxon';
-import { useMe } from '@/hooks/use-me';
-import { PauseWeeks } from './pause-utils';
+import { FC, useEffect, useState } from "react";
+import { apiRequest } from "../../../core/api-request";
+import { BackendCustomer } from "@tnmo/types";
+import MainButton from "@/components/ui/main-button";
+import { DateTime } from "luxon";
+import { useMe } from "@/hooks/use-me";
+import { PauseWeeks } from "./pause-utils";
 
 interface SchedulePauseButtonProps {
   pauseDate: DateTime | null;
@@ -12,39 +12,51 @@ interface SchedulePauseButtonProps {
   handlePauseSelection: () => void;
 }
 
-const SchedulePauseButton: FC<SchedulePauseButtonProps> = ({ pauseDate, resumeDate }) => {
+const SchedulePauseButton: FC<SchedulePauseButtonProps> = ({
+  pauseDate,
+  resumeDate,
+}) => {
   const user = useMe();
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleClick = async () => {
     if (!pauseDate || !resumeDate || !user) return;
     setLoading(true);
-    user.plans.forEach(async plan => pausePlan(plan.id, pauseDate, resumeDate));
-  }
+    user.plans.forEach(async (plan) =>
+      pausePlan(plan.id, pauseDate, resumeDate)
+    );
+  };
 
-  const pausePlan = async (planId: string, pauseDate: DateTime, resumeDate: DateTime) => {
-    const data = await apiRequest<BackendCustomer>("chargebee-pause-plan", {
-      method: "POST",
-      body: JSON.stringify({
-        "plan_id": planId,
-        "pause_date": pauseDate.toUnixInteger(),
-        "resume_date": resumeDate.toUnixInteger()
-      })
-    })
-  }
+  const pausePlan = async (
+    planId: string,
+    pauseDate: DateTime,
+    resumeDate: DateTime
+  ) => {
+    try {
+      await apiRequest<BackendCustomer>("chargebee-pause-plan", {
+        method: "POST",
+        body: JSON.stringify({
+          plan_id: planId,
+          pause_date: pauseDate.toUnixInteger(),
+          resume_date: resumeDate.toUnixInteger(),
+        }),
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
       <MainButton
         disabled={!!(!pauseDate && !user) || loading}
         loading={loading}
-        onClick={() => handleClick()}>
-        {
-          loading ? 'Scheduling...' : 'Schedule this pause'
-        }
+        onClick={() => handleClick()}
+      >
+        {loading ? "Scheduling..." : "Schedule this pause"}
       </MainButton>
     </>
-  )
+  );
 };
 
 export default SchedulePauseButton;
