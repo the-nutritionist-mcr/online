2e9bbc80d6afcb58bd80dc9ef9f648cf0866c898
Ben Wainwright <bwainwright28@gmail.com>
2026-04-10 12:09:12 +0100

    update pause logic to prod

--- src/backend/lambdas/handlers/webhook.ts
+++ src/backend/lambdas/handlers/webhook.ts
@@ -1,13 +1,8 @@
 import "../../utils/init-dd-trace";
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
-import { ENV } from "@tnmo/constants";
-import { handleWebhookProd } from "./handle-webhook-prod";
 import { handleWebhookTest } from "./handle-webhook-test";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-  const environment = process.env[ENV.varNames.EnvironmentName];
-  return environment === "prod"
-    ? handleWebhookProd(event)
-    : handleWebhookTest(event);
+  return handleWebhookTest(event);
 };
